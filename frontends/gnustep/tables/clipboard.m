bb301b99ee515d5e7f349e906f4cacf973e92c84
anthony <anthony@localhost>
2020-12-07 19:48:35 +0000

    Fill in pasteboard functions

--- frontends/gnustep/tables/clipboard.m
+++ frontends/gnustep/tables/clipboard.m
@@ -1,4 +1,5 @@
 #import <Cocoa/Cocoa.h>
+#import <string.h>
 
 #import "netsurf/netsurf.h"
 #import "netsurf/clipboard.h"
@@ -10,11 +11,24 @@
 // Put content of clipboard into buffer up to a maximum length
 static void gnustep_clipboard_get(char **buffer, size_t *length) {
 	NSLog(@"gnustep_clipboard_get");
+	NSString *pb = [[NSPasteboard generalPasteboard] stringForType: NSStringPboardType];
+	char *cstring = "";
+	int len = 0;
+	if (pb) {
+		cstring = [pb cString];
+		len = [pb cStringLength];
+	}
+	*length = len;
+	*buffer = malloc(len + 1);
+	strncpy(*buffer, cstring, len);
+	*buffer[len - 1] = '\0';
 }
 
 // Save the provided clipboard for later retreival above
 static void gnustep_clipboard_set(const char *buffer, size_t length, nsclipboard_styles styles[], int n_styles) {
 	NSLog(@"gnustep_clipboard_set");
+	NSString *pb = [NSString stringWithCString: buffer length: length];
+	[[NSPasteboard generalPasteboard] setString: pb forType: NSStringPboardType];
 }
 
 struct gui_clipboard_table gnustep_clipboard_table = {
